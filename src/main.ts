b4abd60f806409d65da557c1735f3466cd8a2a29
Scanny Mascarenhas <scanny.mascarenhas@gmail.com>
2022-08-10 21:12:10 +0530

    Add debug for context

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import * as core from '@actions/core'
+import * as core from '@actions/core';
+import * as github from '@actions/github';
 
 async function run(): Promise<void> {
   try {
-
+    core.debug(JSON.stringify(github.context));
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
